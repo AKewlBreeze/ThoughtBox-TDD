a29ef44a9fc170fcff7eea1ff641f5882770e753
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-06-29 11:03:01 -0600

    thought card testing completed

--- src/thoughtCard/thoughtCard.js
+++ src/thoughtCard/thoughtCard.js
@@ -3,7 +3,10 @@ import React from 'react';
 export const ThoughtCard = () => {
   return (
     <div>
-    ThoughtCard
+      <div className = 'thoughtCard'>
+        <h1 className = 'title'></h1>
+        <h2 className = 'body'></h2>
+      </div>
     </div>
   )
 }
--- src/thoughtCard/thoughtCard.test.js
+++ src/thoughtCard/thoughtCard.test.js
@@ -6,9 +6,18 @@ import { ThoughtCard } from './thoughtCard.js';
 describe('ThoughtCard', () => {
 
   it('has a thoughtCard class field', () => {
+    const wrapper = shallow(
+      <ThoughtCard />
+    );
+    expect(wrapper.find('.thoughtCard').length).toEqual(1)
   });
 
   it('has a title and body', () => {
+    const wrapper = shallow(
+      <ThoughtCard />
+    )
+    expect(wrapper.find('.title', ).length).toEqual(1)
+    expect(wrapper.find('.body', ).length).toEqual(1)
   });
 
 });
